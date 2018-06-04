b07708f5b6e1d29b12d0fb067be3813591d6f70b
Mariano A. Cortesi <mariano@flowics.com>
2018-06-03 21:44:31 -0300

    fix: inputDefaultBlockFormatter
    The value types of the tests have a missmatch with the actual types the
    formmatter can received.
    
    '0x01' was not declared as a valid type, and yet used here.
    
    We need to decide which is correct.

--- test/formatters.inputDefaultBlockFormatter.ts
+++ test/formatters.inputDefaultBlockFormatter.ts
@@ -1,19 +1,21 @@
 import chai = require('chai')
 const assert = chai.assert
 import * as formatters from '../src/utils/formatters'
+import { Quantity, Tag } from '../src/Schema';
 
-let tests = [
+let tests: { value: Quantity | Tag, expected: any }[] = [
   { value: 'latest', expected: 'latest' },
   { value: 'pending', expected: 'pending' },
   { value: 'earliest', expected: 'earliest' },
   { value: 1, expected: '0x1' },
-  { value: '0x1', expected: '0x1' }
+  // FIXME: should we support this?
+  { value: '0x1' as any, expected: '0x1' }
 ]
 
-describe('lib/web3/formatters', function() {
-  describe('inputDefaultBlockNumberFormatter', function() {
-    tests.forEach(function(test) {
-      it('should turn ' + test.value + ' to ' + test.expected, function() {
+describe('lib/web3/formatters', function () {
+  describe('inputDefaultBlockNumberFormatter', function () {
+    tests.forEach(function (test) {
+      it('should turn ' + test.value + ' to ' + test.expected, function () {
         assert.strictEqual(formatters.inputDefaultBlockNumberFormatter(test.value), test.expected)
       })
     })
