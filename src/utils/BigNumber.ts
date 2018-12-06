fd8327cad3463c57ba79c3351e9eae1d295dada5
belohlavek <dbhvk.96@gmail.com>
2018-12-06 16:25:50 -0300

    chore: use enum

--- src/utils/BigNumber.ts
+++ src/utils/BigNumber.ts
@@ -2,51 +2,51 @@ export type BigNumberValueType = string | number | BigNumber
 
 export type BigNumberRoundingModeType = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
 
-export const BigNumberRoundingMode = {
+export enum BigNumberRoundingMode {
   /**
    * Rounds away from zero.
    */
-  ROUND_UP: 0,
+  ROUND_UP = 0,
 
   /**
    * Rounds towards zero.
    */
-  ROUND_DOWN: 1,
+  ROUND_DOWN = 1,
 
   /**
    * Rounds towards Infinity.
    */
-  ROUND_CE: 2,
+  ROUND_CE = 2,
 
   /**
    * Rounds towards -Infinity.
    */
-  ROUND_FLOOR: 3,
+  ROUND_FLOOR = 3,
 
   /**
    * Rounds towards nearest neighbour. If equidistant, rounds away from zero .
    */
-  ROUND_HALF_UP: 4,
+  ROUND_HALF_UP = 4,
 
   /**
    * Rounds towards nearest neighbour. If equidistant, rounds towards zero.
    */
-  ROUND_HALF_DOWN: 5,
+  ROUND_HALF_DOWN = 5,
 
   /**
    * Rounds towards nearest neighbour. If equidistant, rounds towards even neighbour.
    */
-  ROUND_HALF_EVEN: 6,
+  ROUND_HALF_EVEN = 6,
 
   /**
    * Rounds towards nearest neighbour. If equidistant, rounds towards Infinity.
    */
-  ROUND_HALF_CEIL: 7,
+  ROUND_HALF_CEIL = 7,
 
   /**
    * Rounds towards nearest neighbour. If equidistant, rounds towards -Infinity.
    */
-  ROUND_HALF_FLOOR: 8
+  ROUND_HALF_FLOOR = 8
 }
 
 export interface BigNumber {
