d72b45c90964316adde5fb05dbb5bbbe33102867
Agustin Mendez <agustin.mz.92@gmail.com>
2018-12-13 17:15:19 -0300

    Update esm.ts

--- static/esm.ts
+++ static/esm.ts
@@ -1,8 +1,6 @@
+// tslint:disable
+
 import X from './eth-connect.esm'
-
-// tslint:disable-next-line:whitespace
 type ETHC = typeof import('./eth-connect')
-
 const EthConnect: ETHC = X
-
 export = EthConnect
